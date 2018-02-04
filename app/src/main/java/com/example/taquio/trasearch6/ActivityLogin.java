f1cf2c3276342224611b77076390d70b2e1ecc42
SeanDanielOfficial <officialsuzuen@gmail.com>
2018-02-04 22:14:59 +0800

    Halu

---
+++ app/src/main/java/com/example/taquio/trasearch6/ActivityLogin.java
@@ -0,0 +1,21 @@
+package com.example.taquio.trasearch6;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
+
+/**
+ * Created by Del Mar on 2/4/2018.
+ */
+
+public class ActivityLogin extends AppCompatActivity {
+    EditText username;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log);
+
+        username = (EditText) findViewById(R.id.loginEt2);
+
+    }
+}
