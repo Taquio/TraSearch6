5759011649776879590bcfe38321cba9cf710e54
edmar.greg <edmar.greg@gmail.com>
2018-02-08 10:48:24 +0800

    Merge TEst

---
+++ app/src/main/java/com/example/taquio/trasearch6/Utils/BottomNavigationViewHelper.java
@@ -0,0 +1,67 @@
+package com.example.taquio.trasearch6.Utils;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.util.Log;
+import android.view.MenuItem;
+
+import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
+
+import officialsuzuen.google.com.trasearch.Camera.CameraActivity;
+import officialsuzuen.google.com.trasearch.Home.HomeActivity;
+import officialsuzuen.google.com.trasearch.Messages.MessagesActivity;
+import officialsuzuen.google.com.trasearch.Nearby.NearbyActivity;
+import officialsuzuen.google.com.trasearch.Profile.ProfileActivity;
+import officialsuzuen.google.com.trasearch.R;
+
+/**
+ * Created by Del Mar on 2/7/2018.
+ */
+
+public class BottomNavigationViewHelper {
+    private static final String TAG = "BottomNavigationViewHel";
+
+    public static void setupBottomNavigationView(BottomNavigationViewEx bottomNavigationViewEx) {
+        Log.d(TAG, "setupBottomNavigationView: Setting up BottomNavigationView");
+        bottomNavigationViewEx.enableAnimation(false);
+        bottomNavigationViewEx.enableItemShiftingMode(false);
+        bottomNavigationViewEx.enableShiftingMode(false);
+        bottomNavigationViewEx.setTextVisibility(false);
+    }
+
+    public static void enableNavigation(final Context context, BottomNavigationViewEx view) {
+        view.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                switch (item.getItemId()) {
+
+                    case R.id.ic_home:
+                        Intent intent1 = new Intent(context, HomeActivity.class);
+                        context.startActivity(intent1);
+                        break;
+                    case R.id.ic_messages:
+                        Intent intent2 = new Intent(context, MessagesActivity.class);
+                        context.startActivity(intent2);
+                        break;
+                    case R.id.ic_camera:
+                        Intent intent3 = new Intent(context, CameraActivity.class);
+                        context.startActivity(intent3);
+                        break;
+                    case R.id.ic_nearby:
+                        Intent intent4 = new Intent(context, NearbyActivity.class);
+                        context.startActivity(intent4);
+                        break;
+                    case R.id.ic_profile:
+                        Intent intent5 = new Intent(context, ProfileActivity.class);
+                        context.startActivity(intent5);
+                        break;
+                }
+
+                return false;
+            }
+        });
+    }
+}
