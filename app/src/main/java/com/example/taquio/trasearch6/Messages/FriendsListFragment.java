034c5298a2f64b632466c0b552d298092382edab
SeanDanielOfficial <officialsuzuen@gmail.com>
2018-02-16 01:15:32 +0800

    Like vector

---
+++ app/src/main/java/com/example/taquio/trasearch6/Messages/FriendsListFragment.java
@@ -0,0 +1,27 @@
+package com.example.taquio.trasearch6.Messages;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.taquio.trasearch6.R;
+
+/**
+ * Created by Del Mar on 2/15/2018.
+ */
+
+public class FriendsListFragment extends Fragment {
+    private static final String TAG = "FriendsListFragment";
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_friendslist, container, false);
+
+        return view;
+    }
+}
