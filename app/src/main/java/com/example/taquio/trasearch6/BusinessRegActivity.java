73ef2bdac760f6662aae547e59fb471f3e0a2c4f
SeanDanielOfficial <officialsuzuen@gmail.com>
2018-02-22 09:34:04 +0800

    Merge branches 'Edmar' and 'officialEdward' of https://github.com/Taquio/TraSearch6 into officialEdward
    # Conflicts:
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/java/com/example/taquio/trasearch6/ChooseLayout.java
    #	app/src/main/java/com/example/taquio/trasearch6/HomeActivity2.java
    #	app/src/main/java/com/example/taquio/trasearch6/MessageActivity.java
    #	app/src/main/java/com/example/taquio/trasearch6/RegisterOptions.java
    #	app/src/main/java/com/example/taquio/trasearch6/Utils/ItemsFragment.java
    #	app/src/main/res/drawable-hdpi/ic_action_name.png
    #	app/src/main/res/drawable-mdpi/ic_action_name.png
    #	app/src/main/res/drawable-xhdpi/ic_action_name.png
    #	app/src/main/res/drawable-xxhdpi/ic_action_name.png
    #	app/src/main/res/layout/activity_choose_layout.xml
    #	app/src/main/res/layout/activity_home.xml
    #	app/src/main/res/layout/activity_home2.xml
    #	app/src/main/res/layout/activity_maps.xml
    #	app/src/main/res/layout/activity_registeroptions.xml
    #	app/src/main/res/layout/snippet_top_gallerytoolbar.xml
    #	app/src/main/res/layout/snippet_top_nexttoolbar.xml

--- app/src/main/java/com/example/taquio/trasearch6/BusinessRegActivity.java
+++ app/src/main/java/com/example/taquio/trasearch6/BusinessRegActivity.java
@@ -5,10 +5,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
-
-import com.example.taquio.trasearch6.BusinessHome.BusinessHome;
+import android.widget.EditText;
+import android.widget.Toast;
 
 /**
  * Created by Edward on 20/02/2018.
@@ -17,19 +18,50 @@ import com.example.taquio.trasearch6.BusinessHome.BusinessHome;
 public class BusinessRegActivity extends AppCompatActivity{
 
     private Context mContext = BusinessRegActivity.this;
-    Button busRegister;
+    EditText bsnMail, bsnPass, bsnConPass, bsnBusinessName, bsnLocation, bsnPhone, bsnMobile;
+    Button busContinue;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.business_register);
 
-        busRegister = findViewById(R.id.registerBtn1);
-        busRegister.setOnClickListener(new View.OnClickListener() {
+        bsnMail = (EditText) findViewById(R.id.bsnMail);
+        bsnPass = (EditText) findViewById(R.id.bsnPass);
+        bsnConPass = (EditText) findViewById(R.id.bsnConPass);
+        bsnBusinessName = (EditText) findViewById(R.id.bsnBusinessName);
+        bsnLocation = (EditText) findViewById(R.id.bsnLocation);
+        bsnPhone = (EditText) findViewById(R.id.bsnPhone);
+        bsnMobile = (EditText) findViewById(R.id.bsnMobile);
+
+        busContinue = findViewById(R.id.registerCont);
+        busContinue.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(mContext, BusinessHome.class));
+                String email = bsnMail.getText().toString();
+                String pass = bsnPass.getText().toString();
+                String conpass = bsnConPass.getText().toString();
+                String businessname = bsnBusinessName.getText().toString();
+                String location = bsnLocation.getText().toString();
+                String phone = bsnPhone.getText().toString();
+                String mobile = bsnMobile.getText().toString();
+                if(!TextUtils.isEmpty(email) && !TextUtils.isEmpty(pass) && !TextUtils.isEmpty(conpass) && !TextUtils.isEmpty(businessname)
+                        && !TextUtils.isEmpty(location) && !TextUtils.isEmpty(phone) && !TextUtils.isEmpty(mobile)) {
+
+                    Intent i = new Intent (mContext, BusinessRegActivity2.class);
+                    i.putExtra("EMAIL",email);
+                    i.putExtra("PASS",pass);
+                    i.putExtra("BUSINESSNAME",businessname);
+                    i.putExtra("LOCATION",location);
+                    i.putExtra("PHONE",phone);
+                    i.putExtra("MOBILE",mobile);
+                    startActivity(i);
+                }else {
+                    Toast.makeText(mContext, "Please fill up all fields!", Toast.LENGTH_SHORT).show();
+                }
+
             }
         });
 
     }
+
 }
